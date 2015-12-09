a0a705df9a7629b6d62e7fb97101e39419c50a38
Moein Akbarof <moein.akbarof@trivago.com>
2015-12-09 15:57:45 +0100

    Some more function

--- src/function.php
+++ src/function.php
@@ -4,6 +4,7 @@
 
 // Just for sake of using a constant!
 define('OFFICE_POSITION', 20);
+define('HOME_POSITION', 0);
 
 class Person
 {
@@ -71,6 +72,84 @@ class Person
         }
 
         $vehicle->driveTo(OFFICE_POSITION, function(Vehicle $vehicle, $position){
+            $this->currentPosition = $position;
+
+            //Now we can get in to the vehicle
+            if ($vehicle instanceof VehicleWithDoor)
+            {
+                $vehicle->openDoor();
+            }
+
+            //Go out of the vehicle
+            if ($this->currentForwardLeg === $this->rightLeg)
+            {
+                $this->leftLeg->moveForward();
+                $this->currentForwardLeg = $this->leftLeg;
+                $this->currentPosition++;
+            }
+            else
+            {
+                $this->rightLeg->moveForward();
+                $this->currentForwardLeg = $this->rightLeg;
+                $this->currentPosition++;
+            }
+        });
+    }
+
+    public function goToHome(Vehicle $vehicle)
+    {
+        while ($this->currentPosition != $this->brain->locate($vehicle))
+        {
+            if (is_null($this->currentForwardLeg))
+            {
+                $this->rightLeg->moveForward();
+                $this->currentForwardLeg = $this->rightLeg;
+                $this->currentPosition++;
+                break;
+            }
+            elseif ($this->currentForwardLeg === $this->rightLeg)
+            {
+                $this->leftLeg->moveForward();
+                $this->currentForwardLeg = $this->leftLeg;
+                $this->currentPosition++;
+                break;
+            }
+            else
+            {
+                $this->rightLeg->moveForward();
+                $this->currentForwardLeg = $this->rightLeg;
+                $this->currentPosition++;
+                break;
+            }
+        }
+
+        //Now we can get in to the vehicle
+        if ($vehicle instanceof VehicleWithDoor)
+        {
+            $vehicle->openDoor();
+        }
+
+        //This is supposedly takes us in the vehicle
+        //We already know the $this->currentForwardLeg is not null
+        if ($this->currentForwardLeg === $this->rightLeg)
+        {
+            $this->leftLeg->moveForward();
+            $this->currentForwardLeg = $this->leftLeg;
+            $this->currentPosition++;
+        }
+        else
+        {
+            $this->rightLeg->moveForward();
+            $this->currentForwardLeg = $this->rightLeg;
+            $this->currentPosition++;
+        }
+
+        if ($vehicle instanceof VehicleWithDoor)
+        {
+            $vehicle->closeDoor();
+        }
+
+        $vehicle->driveTo(HOME_POSITION, function(Vehicle $vehicle, $position){
             $this->currentPosition = $position;
 
             //Now we can get in to the vehicle
